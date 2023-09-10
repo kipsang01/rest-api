97c8a7a0f8fd2063b5a90dd8b6e11fe7854306d9
kipsang <kipsang1998@gmail.com>
2023-09-10 17:57:11 +0300

    update env values

--- commerce/settings.py
+++ commerce/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME') or 'orders',
+        'NAME': os.environ.get('DATABASE_NAME'),
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST'),
