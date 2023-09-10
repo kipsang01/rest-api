831d24bbc6d26a7fe9b3c519ca22963823cfa30d
kipsang <kipsang1998@gmail.com>
2023-09-10 18:25:04 +0300

    Add a failing test

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -37,7 +37,7 @@ def test_customer_get_detail(api_client, test_customer_1):
     response = api_client.get(
         reverse_lazy("customer-detail", args=(test_customer_1.id,))
     )
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
 
     data = response.data
     assert isinstance(data, dict)
